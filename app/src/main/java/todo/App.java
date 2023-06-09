73f69c11ac9bcb2308aa3fb4da1da60c5bbddc42
Oyaki122 <oc.yaki122@gmail.com>
2023-06-09 17:20:08 +0900

    読み込み成功

--- app/src/main/java/todo/App.java
+++ app/src/main/java/todo/App.java
@@ -1,26 +1,36 @@
 package todo;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.CodeSource;
-import java.security.ProtectionDomain;
 
 public class App {
+    public static void main(String[] args) {
+        String pwd = System.getProperty("user.dir");
+        Path path = Path.of(pwd, "src/db/test1.sqlite3");
+        final String URL = "jdbc:sqlite:" + path.toString();
+        final String SQL = "select * from syain;";
 
-    public static void main(String[] args) throws Exception {
-        Path path = getApplicationPath(App.class);
-        System.out.println("path=" + path);
-    }
-
-    public static Path getApplicationPath(Class<?> cls) throws URISyntaxException {
-        ProtectionDomain pd = cls.getProtectionDomain();
-        CodeSource cs = pd.getCodeSource();
-        URL location = cs.getLocation();
-        URI uri = location.toURI();
-        Path path = Paths.get(uri);
-        return path;
+        try (Connection conn = DriverManager.getConnection(URL);
+                PreparedStatement ps = conn.prepareStatement(SQL)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    System.out.println(
+                            rs.getInt("id") + " " +
+                                    rs.getString("name") + " " +
+                                    rs.getString("romaji"));
+                }
+            }
+            ;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("処理が完了しました");
+        }
     }
 }
